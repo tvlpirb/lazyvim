30f50aee3226f3248b19e81c93e62e7861400e76
Talhah Peerbhai <talhah.peerbhai@gmail.com>
2025-04-05 02:06:29 +0300

    Initial commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "tiagovla/tokyodark.nvim",
+    lazy = true,
+    opts = {},
+  },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyodark",
+    },
+  },
+}
